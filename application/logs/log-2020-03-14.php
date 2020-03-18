d3b7c6601d08c4bf33fa2df30b29c281b3c41995
hasannurrahmat <hasannurrahmat@gmail.com>
2020-03-18 07:23:47 +0700

    add CI template

---
+++ application/logs/log-2020-03-14.php
@@ -0,0 +1,44 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-03-14 03:49:05 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:49:06 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:49:06 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:49:06 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:49:18 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:49:18 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:49:18 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:50:34 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:50:36 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:51:49 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-03-14 03:52:20 --> The upload path does not appear to be valid.
+ERROR - 2020-03-14 03:52:20 --> Image Upload Error: <p>The upload path does not appear to be valid.</p>
+ERROR - 2020-03-14 03:52:20 --> Severity: Notice --> Undefined index: shop_categorie C:\xampp\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\vendor\models\Products_model.php 49
+ERROR - 2020-03-14 03:52:20 --> Severity: Notice --> Undefined index: brand_id C:\xampp\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\vendor\models\Products_model.php 52
+ERROR - 2020-03-14 03:52:20 --> Query error: Column 'shop_categorie' cannot be null - Invalid query: INSERT INTO `products` (`image`, `shop_categorie`, `quantity`, `position`, `brand_id`, `folder`, `vendor_id`, `time`) VALUES ('', NULL, '1', '1', NULL, '1584154312', '1', 1584154340)
+ERROR - 2020-03-14 03:52:31 --> Could not find the language line "vendor_home_page"
+ERROR - 2020-03-14 03:55:06 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:07 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:07 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:07 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:07 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:07 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:10 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:22 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:22 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:22 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:22 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:22 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:24 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:56:24 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:58:27 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:58:48 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:59:04 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:59:14 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 03:59:54 --> Mailer Error: SMTP connect() failed. https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting
+ERROR - 2020-03-14 03:59:55 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 04:00:02 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 04:00:06 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 04:00:09 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 04:00:11 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 04:01:59 --> 404 Page Not Found: /index
+ERROR - 2020-03-14 04:02:04 --> 404 Page Not Found: /index
